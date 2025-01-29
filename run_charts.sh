d9daabd67bb92a638e74a9c81c36a28316256faa
Evgeny0088 <evgeny.kuznetsov0088@gmail.com>
2025-01-29 16:02:25 +0300

    main: added prometheus chart

--- run_charts.sh
+++ run_charts.sh
@@ -34,6 +34,12 @@ kubectl apply -f ./helm/backend-chart/charts/helm-cert-manager/templates/cert-ma
 kubectl wait --for condition=established crd/certificates.cert-manager.io crd/issuers.cert-manager.io &&\
 kubectl -n cert-manager rollout status deployment.apps/cert-manager-webhook
 
+# install reflector to propagate secrets across namespaces
+#$ kubectl apply -f https://github.com/emberstack/kubernetes-reflector/releases/latest/download/reflector.yaml
+
+# install prometheus-crds
+# kubectl apply server-side -f helm/backend-chart/charts/helm-prometheus-crds/templates
+
 # dry-run
 #helm template --dry-run --debug \
 #    --set global.keycloakNs=keycloak-ns \
@@ -58,6 +64,8 @@ helm upgrade --install \
 #    --set keycloak.keycloakAdminPassword=$KEYCLOAK_ADMIN_PASSWORD \
 #    --set postgres.keycloakDBOwner=$KEYCLOAK_DB_OWNER \
 #    --set postgres.keycloakDBPassword=$KEYCLOAK_DB_PASSWORD \
+#    --set global.keycloakNs=keycloak-ns \
+#    --set global.dnsName=front-keycloak.com \
 #    ./helm/backend-chart/charts/helm-keycloak
 
 # install & upgrade keycloak chart
@@ -87,6 +95,16 @@ helm upgrade --install \
     --set postgres.dbPassword=$USERS_DB_PASSWORD \
     helm-users-db \
     ./helm/backend-chart/charts/helm-users-db
+
+# install & upgrade prometheus-chart
+helm upgrade --install \
+    --atomic \
+    --timeout 30m \
+    --wait \
+    --wait-for-jobs \
+    --namespace backend-ns \
+    helm-prometheus \
+    ./helm/backend-chart/charts/helm-prometheus
 
 # dry-run
 #helm template --dry-run --debug \
